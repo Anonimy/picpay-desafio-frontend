e9c3cafce76d73bc6fa13e3c5e69d13d90b7e573
Anonimy <mateus.larrubia99@gmail.com>
2021-09-26 16:37:42 -0300

    feat: initial configuration + add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
+
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
